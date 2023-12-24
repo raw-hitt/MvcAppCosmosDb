22a305e0fbd6e5b9df1c80210fbe747e2645f749
rohit.pawar <rohit.pawar@hiteshi.com>
2023-12-24 12:53:39 +0530

    roll back

--- Api/EmployeesController.cs
+++ Api/EmployeesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Table;
+//using Microsoft.WindowsAzure.Storage;
+//using Microsoft.WindowsAzure.Storage.Table;
 using MvcAppCosmosDb.Models;
 using MvcAppCosmosDb.Models.Data;
 using MvcAppCosmosDb.Services;
@@ -30,12 +33,17 @@ namespace MvcAppCosmosDb.Api
             {
 
                 #region getting data
-                Database database =  _cosmosClient.GetDatabase("Tablesdb");
-                Container container = database.GetContainer("employees");
+                string connectionString = "DefaultEndpointsProtocol=https;AccountName=rohitpawar;AccountKey=WLjhe30V2efFBdYns9jTpUGaQXJ8ZTaGE8VCrPhMkdqstPAFGVnMHzYpQedephsAWMynm7D1F6rMACDboGKIAA==;TableEndpoint=https://rohitpawar.table.cosmos.azure.com:443/;";
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
 
 
 
-                var query = container.GetItemQueryIterator<Employees>("SELECT * FROM employees");
+
+
+
+
+
                 #endregion
 
 
@@ -65,7 +73,7 @@ namespace MvcAppCosmosDb.Api
             catch (Exception ex)
             {
 
-                _logger.Log(LogLevel.Error, "GetAllEmployees : " + ex.Message);
+                _logger.Log(Microsoft.Extensions.Logging.LogLevel.Error, "GetAllEmployees : " + ex.Message);
                 return new ObjectResult(new ResponseData
                 {
                     Data = "",
